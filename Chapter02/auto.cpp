4916900d9182a1791915727c0d712d4b3eeb5f50
Nikolai Yevik <nkvkyk@gmail.com>
2026-02-10 06:24:24 -0600

    refactor Foo class to use trailing return types for methods

--- Chapter02/auto.cpp
+++ Chapter02/auto.cpp
@@ -22,12 +22,14 @@ namespace new_syntax {
 
 class Foo {
 public:
-  auto val() const { return m_; }
+  auto val() const -> int { return m_; }
 
   auto &cref() const { return m_; }
+  auto cref_trailing() const -> const int& { return m_; }
   auto &mref() { return m_; }
+  auto mref_trailing() -> int& { return m_; }
 
-private:
+private: 
   int m_{};
 };
 
@@ -36,7 +38,9 @@ private:
 TEST(Auto, ConstAndMutableReference) {
   auto foo = new_syntax::Foo{};
   auto &cref = foo.cref(); // cref is a const reference
+  auto &cref_trailing = foo.cref_trailing(); // cref_trailing is also a const reference
   auto &mref = foo.mref(); // mref is a mutable reference
+  mref =foo.mref_trailing();
 
   static_assert(std::is_const_v<std::remove_reference_t<decltype(cref)>>, "");
   static_assert(!std::is_const_v<std::remove_reference_t<decltype(mref)>>, "");
